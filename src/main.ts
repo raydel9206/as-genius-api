3db964d645633cba1a095d045028949b6ccfee8f
jadecardenas <37634705+jadecardenas93@users.noreply.github.com>
2024-01-30 14:28:38 +0100

    fix: deploy routes

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
-  app.setGlobalPrefix('api/v1');
-
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
